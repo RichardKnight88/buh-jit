4d2bbe3317d234bd232e00ea3b90c1f8f1ba7299
richardknight <rknightdesign@gmail.com>
2021-07-23 10:41:31 +0100

    user model created

--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.CharField(max_length=50, unique=True)
+    username = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    profile_image = models.CharField(max_length=500, blank=True)
+    balance = models.FloatField(blank=True, default=0.0)
+    transactions = models.CharField(max_length=10, blank=True)
+    families = models.CharField(max_length=10, blank=True)
+    credits = models.CharField(max_length=500, blank=True)
