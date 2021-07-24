c0efe537797607588345755c5bd6951cc17f7d3f
richardknight <rknightdesign@gmail.com>
2021-07-24 17:30:03 +0100

    household edit added.

--- household/views.py
+++ household/views.py
@@ -1,3 +1,4 @@
+from functools import partial
 from rest_framework.views import APIView 
 from rest_framework.response import Response 
 from rest_framework import status
@@ -37,5 +38,22 @@ class HouseholdIndividualView(APIView):
 
     def get(self, _request, pk):
         individual_household = self.get_household(pk=pk)
+
         serialized_individual_household = PopulatedHouseholdSerializer(individual_household)
         return Response(serialized_individual_household.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        
+        household_to_edit = self.get_household(pk=pk)
+        changes_to_add = request.data
+        serialized_updated_household = HouseholdSerializer(household_to_edit, data=changes_to_add, partial=True)
+
+        if household_to_edit.created_by != request.user:
+            raise PermissionDenied()
+        
+        if serialized_updated_household.is_valid():
+
+            serialized_updated_household.save()
+            return Response(serialized_updated_household.data, status=status.HTTP_202_ACCEPTED)
+        
+        return Response(serialized_updated_household.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
