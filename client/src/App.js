360fd418a36b2e48cc9c8413e5dff85bce713d47
richardknight <rknightdesign@gmail.com>
2021-07-24 19:19:25 +0100

    router added to App. Navbar component added

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,42 @@
 import React, { useEffect } from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import axios from 'axios'
+import Navbar from './components/Navbar'
 
 const App = () => {
 
-  useEffect(() => {
-    const getData = async () => {
-      const { data } = await axios('/api/household/1') // * <-- replace with your endpoint
-      // const data = await res.json()
-      console.log(data)
-    }
-    getData()
-  },[])
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const { data } = await axios('/api/household/1') // * <-- replace with your endpoint
+  //     // const data = await res.json()
+  //     console.log(data)
+  //   }
+  //   getData()
+  // }, [])
 
-  return <h1>Hello World</h1>
+  return (
+    <>
+
+      <Navbar />
+
+      <BrowserRouter>
+
+        <Switch>
+
+
+          <Route path='/'>
+            <h1>Hello World</h1>
+          </Route>
+
+
+        </Switch>
+
+      </BrowserRouter>
+
+
+    </>
+  )
+
 }
 
 export default App
