cb3442c490a47dd5804391838fd8c70e30cfd64d
richardknight <rknightdesign@gmail.com>
2021-08-10 17:58:26 +0100

    reoriented settings imports

--- project/settings.py
+++ project/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import django_on_heroku
 import os
+import django_on_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
