dfa4606de8cc34c5b3b649f19e0a3b842351f084
richardknight <rknightdesign@gmail.com>
2021-08-10 17:46:51 +0100

    change to setting to have DIRS on one line

--- project/settings.py
+++ project/settings.py
@@ -59,9 +59,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'client')
-                ]  #Look, we have added the root folder of frontend here
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'client')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
